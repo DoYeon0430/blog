a8becfe4140f49ae32af54491f01b57afe7ebb7c
DoYeon0430 <kim73916@naver.com>
2023-05-17 01:37:30 +0900

    debug test upload3

--- config/settings/base.py
+++ config/settings/base.py
@@ -26,11 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','54.225.71.199']
 
-
+DEBUG = True
 
 # Application definition
 
