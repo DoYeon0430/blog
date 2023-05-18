f3f63a5cfe21802b117e3654057b8416bf268cf3
DoYeon0430 <kim73916@naver.com>
2023-05-18 15:48:33 +0900

    home index upload

--- myprofile/models.py
+++ myprofile/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from movie.models import Movie
 from engineer.models import Django
 
@@ -13,3 +15,13 @@ class List_django(models.Model):
 
     def __str__(self):
         return str(self.django.subject)
+
+@receiver(post_save, sender=Movie) #참조키 값 생성될 때 자동 생성
+def create_list_movie(sender, instance, created, **kwargs):
+    if created:
+        List_movie.objects.create(movie=instance)
+
+@receiver(post_save, sender=Django) #참조키 값 생성될 때 자동 생성
+def create_list_django(sender, instance, created, **kwargs):
+    if created:
+        List_django.objects.create(django=instance)
