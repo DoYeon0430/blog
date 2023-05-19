10e0eb7437f4781f70236587c57d85bbd83a5394
DoYeon0430 <kim73916@naver.com>
2023-05-20 05:35:23 +0900

    iP upload.

--- mywork/views.py
+++ mywork/views.py
@@ -23,7 +23,15 @@ def main(request):
 
 def detail(request, mywork_id):
     detail = get_object_or_404(Mywork, pk=mywork_id)
-    client_ip = request.META['REMOTE_ADDR']
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        # X-Forwarded-For 헤더 값이 존재하는 경우 IP 주소를 추출합니다.
+        # X-Forwarded-For 헤더는 쉼표로 구분된 IP 주소 목록이며, 일반적으로 첫 번째 IP 주소가 클라이언트의 주소입니다.
+        ip_list = x_forwarded_for.split(',')
+        client_ip = ip_list[0].strip()
+    else:
+        # X-Forwarded-For 헤더가 존재하지 않는 경우, REMOTE_ADDR을 사용하여 클라이언트의 IP 주소를 가져옵니다.
+        client_ip = request.META.get('REMOTE_ADDR')
     context = {
         'detail': detail,
         'client_ip': client_ip,
