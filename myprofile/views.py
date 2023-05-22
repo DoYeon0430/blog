afa799275fa4ea7e0c01e35ccdde0d6bae6c95a9
DoYeon0430 <kim73916@naver.com>
2023-05-23 07:14:48 +0900

    cookie create upload

--- myprofile/views.py
+++ myprofile/views.py
@@ -8,6 +8,46 @@ def home(request):
     tag = request.GET.get('tag')
     main_view = Views.objects.get(pk=3)
 
+    cookie_name = f'main_'
+    if cookie_name not in request.COOKIES:
+        main_view.count += 1
+        main_view.save()
+
+        expires = datetime.utcnow() + timedelta(days=1)
+        expires = expires.strftime('%a, %d-%b-%Y %H:%M:%S GMT')
+
+        mywork_content = List_mywork.objects.all()
+        reversed_mywork_content = list(reversed(mywork_content))
+        movie_content = List_movie.objects.all()
+        reversed_movie_content = list(reversed(movie_content))
+        physics_content = List_physics.objects.all()
+        reversed_physics_content = list(reversed(physics_content))
+        django_content = List_django.objects.all()
+        reversed_django_content = list(reversed(django_content))
+        network_content = List_network.objects.all()
+        reversed_network_content = list(reversed(network_content))
+
+        reversed_mywork_count = len(reversed_mywork_content)
+        reversed_movie_count = len(reversed_movie_content)
+        reversed_physics_count = len(reversed_physics_content)
+        reversed_django_count = len(reversed_django_content)
+        reversed_network_count = len(reversed_network_content)
+
+        content = {
+            'reversed_movie_content': reversed_movie_content,
+            'reversed_mywork_content': reversed_mywork_content,
+            'reversed_mywork_count': reversed_mywork_count,
+            'reversed_movie_count': reversed_movie_count,
+            'reversed_django_count': reversed_django_count,
+            'reversed_physics_count': reversed_physics_count,
+            'reversed_network_count': reversed_network_count,
+            'main_view': main_view
+        }
+
+        response = HttpResponse(render(request, 'myprofile/home.html', content))
+        response.set_cookie(cookie_name, 'true', expires=expires)
+        return response
+
     if tag == '대표글':
         mywork_content = List_mywork.objects.all()
         reversed_mywork_content = list(reversed(mywork_content))
