49d1054b05021c8a09e77f9065f99221567de271
DoYeon0430 <kim73916@naver.com>
2023-05-31 02:37:11 +0900

    model meta1 upload

--- movie/models.py
+++ movie/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 class Movie(models.Model):
     subject = models.CharField(max_length=200)
     content = models.CharField(max_length=200)
-    meta = models.CharField(max_length=80)
+    meta = models.CharField(max_length=130)
     htmlcontent = HTMLField()
     screening = models.DateTimeField()
     create_date = models.DateTimeField(auto_now_add=True)
