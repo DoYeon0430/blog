e96cbe8723ee8962bdb2455208fb749a7a5b6f6e
DoYeon0430 <kim73916@naver.com>
2023-05-30 14:27:59 +0900

    url error upload

--- engineer/urls.py
+++ engineer/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('django/',views.django_main, name='django_main'),
     path('network/',views.network_main, name='network_main'),
     path('physics/',views.physics_main, name='physics_main'),
-    path('physics/<int:study_id>',views.physics, name='physics'),
-    path('django/<int:study_id>',views.django, name='django'),
-    path('network/<int:study_id>',views.network, name='network'),
+    path('physics/<int:study_id>/',views.physics, name='physics'),
+    path('django/<int:study_id>/',views.django, name='django'),
+    path('network/<int:study_id>/',views.network, name='network'),
 ]
