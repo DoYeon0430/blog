dc87db54bfd2d1abdeb1e96a9ab7552138aeb424
DoYeon0430 <kim73916@naver.com>
2023-06-10 13:51:04 +0900

    navbar remake11 upload

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -1,11 +1,7 @@
 var dropdown = document.querySelector(".dropdown_bar");
 var dropdownContent = document.querySelector(".dropdown-content");
-var dropdown_toggle = document.querySelector(".dropdown_toggle");
-var dropdown_toggleContent = document.querySelector(".dropdown_main");
 var drop_icon1 = document.querySelector(".drop_icon1");
 var drop_icon2 = document.querySelector(".drop_icon2");
-var drop_toggle_icon1 = document.querySelector(".drop_toggle_icon1");
-var drop_toggle_icon2 = document.querySelector(".drop_toggle_icon2");
 
 dropdown.addEventListener("click", function() {
   if (dropdownContent.style.display === "block") {
@@ -17,17 +13,4 @@ dropdown.addEventListener("click", function() {
       drop_icon1.style.display = "none";
       drop_icon2.style.display = "inline-flex";
   }
-});
-
-
-dropdown_toggle.addEventListener("click", function() {
-    if (dropdown_toggleContent.style.display === "block") {
-      dropdown_toggleContent.style.display = "none";
-      drop_toggle_icon1.style.display = "inline-flex";
-      drop_toggle_icon2.style.display = "none";
-    } else {
-      dropdown_toggleContent.style.display = "block";
-      drop_toggle_icon1.style.display = "none";
-      drop_toggle_icon2.style.display = "inline-flex";
-    }
 });
