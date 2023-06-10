1a0784547445da3f5396252bb95ff61667cf54ae
DoYeon0430 <kim73916@naver.com>
2023-06-10 12:45:18 +0900

    navbar remake7 upload

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -2,14 +2,21 @@ var dropdown = document.querySelector(".dropdown_bar");
 var dropdownContent = document.querySelector(".dropdown-content");
 var dropdown_toggle = document.querySelector(".dropdown_toggle");
 var dropdown_toggleContent = document.querySelector(".dropdown_main");
+var drop_icon1 = document.querySelector(".drop_icon1");
+var drop_icon2 = document.querySelector(".drop_icon2");
 
 dropdown.addEventListener("click", function() {
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-    } else {
-        dropdownContent.style.display = "block";
-    }
+  if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+      drop_icon1.style.display = "inline-flex";
+      drop_icon2.style.display = "none";
+  } else {
+      dropdownContent.style.display = "block";
+      drop_icon1.style.display = "none";
+      drop_icon2.style.display = "inline-flex";
+  }
 });
+
 
 dropdown_toggle.addEventListener("click", function() {
     if (dropdown_toggleContent.style.display === "block") {
