469bc6a537cc4bfec3c408d1d600121d87b4c4bd
DoYeon0430 <kim73916@naver.com>
2023-06-16 03:47:00 +0900

    sidebar modify1 upload

--- engineer/migrations/0026_django_code.py
+++ engineer/migrations/0026_django_code.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('engineer', '0025_alter_django_meta_alter_network_meta_and_more'),
+        ('engineer', '0024_alter_django_meta_alter_network_meta_and_more'),
     ]
 
     operations = [
