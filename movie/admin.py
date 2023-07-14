42c905f0ae49df803dd71fae53e4aa40d1be0a19
DoYeon0430 <kim73916@naver.com>
2023-07-14 22:22:15 +0900

    admin new upload

--- movie/admin.py
+++ movie/admin.py
@@ -3,8 +3,10 @@ from .models import Movie, Comment
 
 
 class MovieAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'meta', 'create_date')
     actions = ['delete_selected']
     search_fields = ['subject', 'content']
+    list_filter = ('create_date','genre')
 
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Comment)
