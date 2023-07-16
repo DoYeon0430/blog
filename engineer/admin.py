e1e1333d4d85b7986eda8cd83d412f090a194860
DoYeon0430 <kim73916@naver.com>
2023-07-16 09:38:06 +0900

    engineer admin renew upload

--- engineer/admin.py
+++ engineer/admin.py
@@ -1,33 +1,71 @@
 from django.contrib import admin
-from django import forms
 from .models import Physics, Django, Network, Comment_physics, Comment_django, Comment_network
-from tinymce.widgets import TinyMCE
-from django.db import models
+import html
+from django.utils.html import strip_tags
 
 
 # Post 클래스는 해당하는 Photo 객체를 리스트로 관리하는 한다. 
 class PhysicsAdmin(admin.ModelAdmin):
+    list_display = ('get_subject','display_htmlcontent', 'display_meta', 'get_formatted_create_date')
     search_fields = ['subject', 'content']
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'engineerkey':
-            kwargs['empty_label'] = '참조키를 입력하시오.'
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    def get_subject(self, obj):
+        return obj.subject
+    get_subject.short_description = '제목'
+
+    def display_htmlcontent(self, obj):
+        decoded_content = html.unescape(strip_tags(obj.htmlcontent))
+        return decoded_content[:40] if len(decoded_content) > 40 else decoded_content
+    display_htmlcontent.short_description = 'HTML 내용'
+    
+    def display_meta(self, obj):
+        return strip_tags(obj.meta)[:40] if len(strip_tags(obj.meta)) > 40 else strip_tags(obj.meta)
+    display_meta.short_description = 'META 태그'
+
+    def get_formatted_create_date(self, obj):
+        return obj.create_date.strftime('%Y-%m-%d')
+    get_formatted_create_date.short_description = '날짜'
     
 
 class DjangoAdmin(admin.ModelAdmin):
+    list_display = ('get_subject','display_htmlcontent', 'display_meta', 'get_formatted_create_date')
     search_fields = ['subject', 'content']
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'engineerkey':
-            kwargs['empty_label'] = '참조키를 입력하시오.'
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    def get_subject(self, obj):
+        return obj.subject
+    get_subject.short_description = '제목'
+
+    def display_htmlcontent(self, obj):
+        decoded_content = html.unescape(strip_tags(obj.htmlcontent))
+        return decoded_content[:40] if len(decoded_content) > 40 else decoded_content
+    display_htmlcontent.short_description = 'HTML 내용'
+    
+    def display_meta(self, obj):
+        return strip_tags(obj.meta)[:40] if len(strip_tags(obj.meta)) > 40 else strip_tags(obj.meta)
+    display_meta.short_description = 'META 태그'
+
+    def get_formatted_create_date(self, obj):
+        return obj.create_date.strftime('%Y-%m-%d')
+    get_formatted_create_date.short_description = '날짜'
 
 
 class NetworkAdmin(admin.ModelAdmin):
+    list_display = ('get_subject','display_htmlcontent', 'display_meta', 'get_formatted_create_date')
     search_fields = ['subject', 'content']
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'engineerkey':
-            kwargs['empty_label'] = '참조키를 입력하시오.'
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    def get_subject(self, obj):
+        return obj.subject
+    get_subject.short_description = '제목'
+
+    def display_htmlcontent(self, obj):
+        decoded_content = html.unescape(strip_tags(obj.htmlcontent))
+        return decoded_content[:40] if len(decoded_content) > 40 else decoded_content
+    display_htmlcontent.short_description = 'HTML 내용'
+    
+    def display_meta(self, obj):
+        return strip_tags(obj.meta)[:40] if len(strip_tags(obj.meta)) > 40 else strip_tags(obj.meta)
+    display_meta.short_description = 'META 태그'
+
+    def get_formatted_create_date(self, obj):
+        return obj.create_date.strftime('%Y-%m-%d')
+    get_formatted_create_date.short_description = '날짜'
     
 
 admin.site.register(Physics, PhysicsAdmin)
