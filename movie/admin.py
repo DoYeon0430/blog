690d7ac8415b17cac6919b85953e39bbc37f8be1
DoYeon0430 <kim73916@naver.com>
2023-07-29 06:58:49 +0900

    a tag renew upload

--- movie/admin.py
+++ movie/admin.py
@@ -4,27 +4,18 @@ import html
 from django.utils.html import strip_tags
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('get_subject','display_htmlcontent', 'display_meta', 'get_formatted_create_date')
+    list_display = ('get_subject_with_content', 'display_meta', 'get_formatted_create_date')
     actions = ['delete_selected']
     search_fields = ['subject', 'content']
     list_filter = ('create_date', 'genre')
 
-    def get_subject(self, obj):
-        return obj.subject
-    get_subject.short_description = '제목'
-
-    def display_htmlcontent(self, obj):
-        stripped_content = strip_tags(obj.htmlcontent)
-        decoded_content = html.unescape(stripped_content)
-        genre_index = decoded_content.find('<주연>')  # 특정 텍스트(`<장르>`)의 인덱스 검색
-        content_before_genre = decoded_content[:genre_index] if genre_index != -1 else decoded_content
-        return content_before_genre
-    display_htmlcontent.short_description = '본문 내용'
+    def get_subject_with_content(self, obj):
+        return f"{obj.content}, <{obj.subject}> 후기"
+    get_subject_with_content.short_description = '제목'
 
     def display_meta(self, obj):
         return obj.meta[:60] if len(obj.meta) > 60 else obj.meta
     display_meta.short_description = 'META 태그'
-
 
     def get_formatted_create_date(self, obj):
         return obj.create_date.strftime('%Y-%m-%d')
@@ -32,4 +23,5 @@ class MovieAdmin(admin.ModelAdmin):
 
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Comment)
+
 
