02d32ada022b9125f0c95028596555cdd8264834
DoYeon0430 <kim73916@naver.com>
2023-08-17 21:09:32 +0900

    network select renew16 upload

--- engineer/views.py
+++ engineer/views.py
@@ -148,9 +148,21 @@ def physics_main(request):
     view = Views.objects.get(pk=3)
 
     tag = request.GET.get('tag','')
+    selected_value = tag
     page = request.GET.get('page','1')
     kw = request.GET.get('kw','')
-    physics_list = Physics.objects.all().order_by('-create_date') # 최신순으로 정렬하여 가져옴
+
+    physics_list = Physics.objects.all().none()
+    page_obj = None
+
+    if tag == 'ROS':
+        physics_list = Physics.objects.filter(code='ROS').order_by('-create_date')
+    elif tag == '이론':
+        physics_list = Physics.objects.filter(code='이론').order_by('-create_date')
+    elif tag == '대학교':
+        physics_list = Physics.objects.filter(code='대학교').order_by('-create_date')
+    else:
+        physics_list = Physics.objects.order_by('-create_date')
 
     if kw:
         physics_list = physics_list.filter(
@@ -183,7 +195,8 @@ def physics_main(request):
         'physics_list':physics_list, 
         'page':page, 
         'kw':kw,
-        'url': url
+        'url': url,
+        'selected_value':selected_value
         })
 
 
@@ -433,9 +446,21 @@ def network_main(request):
     view = Views.objects.get(pk=3)
 
     tag = request.GET.get('tag','')
+    selected_value = tag
     page = request.GET.get('page','1')
     kw = request.GET.get('kw','')
-    network_list = Network.objects.all().order_by('-create_date') # 최신순으로 정렬하여 가져옴
+
+    network_list = Network.objects.none()
+    page_obj = None
+
+    if tag == '공군':
+        network_list = Network.objects.filter(code='공군').order_by('-create_date')
+    elif tag == '네트워크':
+        network_list = Network.objects.filter(code='네트워크').order_by('-create_date')
+    elif tag == '자격증':
+        network_list = Network.objects.filter(code='자격증').order_by('-create_date')
+    else:
+        network_list = Network.objects.order_by('-create_date')
 
     if kw:
         network_list = network_list.filter(
@@ -468,7 +493,8 @@ def network_main(request):
         'network_list':network_list, 
         'page':page, 
         'kw':kw,
-        'url': url
+        'url': url,
+        'selected_value':selected_value
         })
 
 @csrf_exempt
