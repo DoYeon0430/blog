4526d38a6d50a3248ec3e63697f28f982f007f02
DoYeon0430 <kim73916@naver.com>
2024-03-08 18:23:06 +0900

    sercet renew19 upload

--- myprofile/forms.py
+++ myprofile/forms.py
@@ -4,11 +4,12 @@ from .models import MeetingDate
 class MeetingDateForm(forms.ModelForm):
     class Meta:
         model = MeetingDate
-        fields = ['date', 'text', 'details', 'photo']
+        fields = ['date', 'text', 'details', 'description']
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date', 'placeholder': '날짜를 입력하세요'}),
             'text': forms.TextInput(attrs={'placeholder': 'ex) 서울시 은평구'}),
             'details': forms.TextInput(attrs={'placeholder': 'ex) 연신내 달빛포차'}),
+            'description': forms.Textarea(attrs={'placeholder': '이미지 설명을 입력하세요'}),
         }
 
 class ImageForm(forms.ModelForm):
